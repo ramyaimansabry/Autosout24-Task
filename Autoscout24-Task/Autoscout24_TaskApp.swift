1c563e4458ac60484df9a8d749dcf6acbb7ce9db
Ramy Sabry <ramy.sabry@fawry.com>
2023-04-07 21:49:10 +0200

    Initial Commit

---
+++ Autoscout24-Task/Autoscout24_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Autoscout24_TaskApp.swift
+//  Autoscout24-Task
+//
+//  Created by Ramy Sabry on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Autoscout24_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
